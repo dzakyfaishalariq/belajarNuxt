11e5d8f8c6e20cd7972da9d911f73bf09f566473
dzakyfaishalariq <dzakyfaishalariq@gmail.com>
2025-11-10 16:52:31 +0700

    belajar nuxt

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,5 +1,35 @@
+<script setup lang="ts">
+const fetur = ref([
+  {
+    title: "Happy",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eaque repellendus eveniet error autem, assumenda doloribus repellat ratione voluptas debitis cum neque veniam, commodi placeat consectetur ullam excepturi id nisi.",
+    icon: "i-lucide-smile",
+    to: "/",
+  },
+  {
+    title: "Semangat",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eaque repellendus eveniet error autem, assumenda doloribus repellat ratione voluptas debitis cum neque veniam, commodi placeat consectetur ullam excepturi id nisi.",
+    icon: "i-lucide-a-large-small",
+    to: "/",
+  },
+  {
+    title: "Taqwa",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eaque repellendus eveniet error autem, assumenda doloribus repellat ratione voluptas debitis cum neque veniam, commodi placeat consectetur ullam excepturi id nisi.",
+    icon: "i-lucide-sun-moon",
+    to: "/",
+  },
+]);
+</script>
 <template>
   <div>
-    <h1>ABout</h1>
+    <UPageSection
+      title="About"
+      description="Aplikasi ini hanya untuk perkenalan saja dan merupakan aplikasi untuk uji coba"
+      icon="i-lucide-rocket"
+      :features="fetur"
+    />
   </div>
 </template>
